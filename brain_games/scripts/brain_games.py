7cb5ac087a36c6c1d5ea7105f322d02aaf2db57c
NevermoreKatana <crymorebch@gmail.com>
2023-06-05 12:06:57 +0300

    fix for flake

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 from brain_games import cli
+
+
 def greeting():
     print('Welcome to the Brain Games!')
+
 
 def main():
     greeting()
     cli.welcome_user()
 
+
 if __name__ == '__main__':
     main()
-    
