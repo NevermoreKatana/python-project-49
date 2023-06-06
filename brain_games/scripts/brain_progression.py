2667d95c479e9bb472749e0e7a264c383fedbbba
Katana Nevermore <128977921+NevermoreKatana@users.noreply.github.com>
2023-06-06 09:10:20 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -5,19 +5,17 @@ from .brain_games import greeting
 
 
 def generate_prog():
-    massive = []
-    s = ''
+    progression = []
     first_number = random.randint(1, 20)
     number_of_progression = random.randint(1, 30)
-    massive.append(first_number)
-    for i in range(9):
-        massive.append(massive[i] + number_of_progression)
-    answer_numb = random.choice(massive)
-    index = random.choice([i for i in range(len(massive)) if massive[i] != answer_numb])
-    massive[index] = '..'
-    for i in range(len(massive)):
-        s += ' ' + str(massive[i])
-    return str(answer_numb), s
+    progression.append(first_number)
+    for _ in range(9):
+        progression.append(progression[-1] + number_of_progression)
+    hidden_index = random.randint(0, 9)
+    answer = progression[hidden_index]
+    progression[hidden_index] = '..'
+    question = ' '.join(str(num) for num in progression)
+    return str(answer), question
 
 
 def prog_game(name: str):
@@ -26,15 +24,14 @@ def prog_game(name: str):
     while True:
         if counter == 3:
             return f'Congratulations, {name}!'
-        response, ask = generate_prog()
-        print(f'Question: {ask}')
+        response, question = generate_prog()
+        print(f'Question: {question}')
         answer = prompt.string("Your answer: ")
         if answer == response:
             print('Correct!')
             counter += 1
         else:
-            return f"'{answer}' is wrong answer ;(. Correct answer was \
-'{response}'\nLet's try again, {name}!"
+            return f"'{answer}' is the wrong answer ;(. The correct answer was '{response}'.\nLet's try again, {name}!"
 
 
 def main():
