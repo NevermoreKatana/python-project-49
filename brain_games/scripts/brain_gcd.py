35d3481a36942f607c46f65795de839aa61de649
Katana Nevermore <128977921+NevermoreKatana@users.noreply.github.com>
2023-06-07 11:41:14 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -20,15 +20,11 @@ def nod_game(name: str):
             print('Correct!')
             counter += 1
         else:
-            return f"'{answer}' is wrong answer ;(. Correct answer was \
-'{response}'\nLet's try again, {name}!"
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was \
+'{response}'\nLet's try again, {name}!")
 
 
 def main():
     greeting()
     name = cli.welcome_user()
     nod_game(name)
-
-
-if __name__ == '__main__':
-    main()
