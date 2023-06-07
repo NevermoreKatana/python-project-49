0e4912620955bc282c1d863496857258fc0b81b1
Katana Nevermore <128977921+NevermoreKatana@users.noreply.github.com>
2023-06-07 13:32:39 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,15 +3,19 @@ from brain_games import cli
 from .brain_games import greeting
 
 
+def check_number(number: int):
+    if number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
 def parity_check_game(name: str):
     print('Answer "yes" if the number is even, otherwise answer "no".')
     counter = 0
     while counter < 3:
         number = random.randint(1, 50)
-        if number % 2 == 0:
-            response = 'yes'
-        else:
-            response = 'no'
+        response = check_number(number)
         print(f'Question: {number}')
         answer = input("Your answer: ").lower()
         if answer == response:
