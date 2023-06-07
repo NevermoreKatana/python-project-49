f6fe1596779b752f067038d1921ef0f28b5b6c90
Katana Nevermore <128977921+NevermoreKatana@users.noreply.github.com>
2023-06-07 11:41:32 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -33,8 +33,8 @@ def prime_game(name: str):
             print('Correct!')
             counter += 1
         else:
-            return f"'{answer}' is wrong answer ;(. Correct answer was \
-'{response}'\nLet's try again, {name}!"
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was \
+'{response}'\nLet's try again, {name}!")
 
 
 def main():
