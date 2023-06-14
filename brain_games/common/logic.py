db3eeb012c2ad9e83df1638c62e40108fa60cba0
NevermoreKatana <crymorebch@gmail.com>
2023-06-14 14:06:25 +0300

    Constants are placed in global variables and "magic numbers" are corrected

--- brain_games/common/logic.py
+++ brain_games/common/logic.py
@@ -1,6 +1,9 @@
 import random
 
 
+NUMBER_OF_ROUNDS = 3
+
+
 def correct_answer(response):
     if response is True:
         return 'yes'
@@ -10,11 +13,10 @@ def correct_answer(response):
 def play_game(name, game_title, is_correct):
     print(game_title)
     counter = 0
-    NUMBER_OF_ROUNDS = 3
     while counter < NUMBER_OF_ROUNDS:
-        number = random.randint(1, 50)
-        response = is_correct(number)
-        print(f'Question: {number}')
+        FIRST_NUMBER_FOR_GAME = random.randint(1, 50)
+        response = is_correct(FIRST_NUMBER_FOR_GAME)
+        print(f'Question: {FIRST_NUMBER_FOR_GAME}')
         answer = input("Your answer: ").lower()
         if (answer == 'yes' and response) or (answer == 'no' and not response):
             print('Correct!')
@@ -31,13 +33,12 @@ Correct answer was '{correct_answer(response)}'")
 def play_game_two_numbers(name, game_title, is_correct, quest):
     print(game_title)
     counter = 0
-    NUMBER_OF_ROUNDS = 3
     while counter < NUMBER_OF_ROUNDS:
-        first_number = random.randint(1, 50)
-        second_number = random.randint(1, 50)
         question = quest()
-        response = is_correct(first_number, second_number, question)
-        print(f'Question: {first_number} {question} {second_number}')
+        FIRST_NUMBER_FOR_GAME = random.randint(1, 50)
+        SECOND_NUMBER_FOR_GAME = random.randint(1, 50)
+        response = is_correct(FIRST_NUMBER_FOR_GAME, SECOND_NUMBER_FOR_GAME, question)
+        print(f'Question: {FIRST_NUMBER_FOR_GAME} {question} {SECOND_NUMBER_FOR_GAME}')
         answer = input("Your answer: ").lower()
         if str(response) == answer:
             print('Correct!')
