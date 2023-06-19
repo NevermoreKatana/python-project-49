4fa3549364c378b9f107f7c724a63ad3a6972818
NevermoreKatana <crymorebch@gmail.com>
2023-06-19 11:17:23 +0300

    Linter

--- brain_games/common/logic.py
+++ brain_games/common/logic.py
@@ -15,12 +15,14 @@ def play_game(name, game_title, func_gen_question):
         response, question = func_gen_question()
         print(question)
         answer = input("Your answer: ").lower()
-        if (answer == 'yes' and response) or (answer == 'no' and not response) or (answer == str(response)):
+        if (answer == 'yes' and response) or (answer == 'no' and not response)\
+                or (answer == str(response)):
             print('Correct!')
         elif (answer == str(response)):
             print('Correct!')
         else:
-            print(f"'{answer}' is the wrong answer ;(. Correct answer was '{correct_answer(response)}'")
+            print(f"'{answer}' is the wrong answer ;(. "
+                  f"Correct answer was '{correct_answer(response)}'")
             print(f"Let's try again, {name}!")
             break
     else:
