4012e536e06fe7fb634d5ba712b33459c38300eb
NevermoreKatana <crymorebch@gmail.com>
2023-06-21 11:32:58 +0300

    logic refactor

--- brain_games/common/logic.py
+++ brain_games/common/logic.py
@@ -7,12 +7,10 @@ def play_game(name, game_title, func_gen_question):
         response, question = func_gen_question()
         print(question)
         answer = input("Your answer: ").lower()
-        if (answer == response):
-            print('Correct!')
-        else:
+        if (answer != response):
             print(f"'{answer}' is the wrong answer ;(. "
                   f"Correct answer was '{response}'")
             print(f"Let's try again, {name}!")
-            break
-    else:
-        print(f'Congratulations, {name}!')
+            return
+        print('Correct!')
+    print(f'Congratulations, {name}!')
