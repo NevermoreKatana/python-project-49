31c372013c58f605c9ce9face2958b796f82264b
NevermoreKatana <crymorebch@gmail.com>
2023-06-24 23:00:47 +0300

    fix

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from brain_games.game_utilities.utils import start_game, initialize_game
+from brain_games.game_utilities.utils import start_game
 from brain_games.games.progression import TITLE, create_expression
 
 
 def main():
-    name = initialize_game()
-    start_game(name, TITLE, create_expression)
+    start_game(TITLE, create_expression)
