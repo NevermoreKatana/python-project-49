222741dfeb9cb9f7a09f37bcf4752f57d34ec161
NevermoreKatana <crymorebch@gmail.com>
2023-06-27 16:08:07 +0300

    fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,7 +7,7 @@ MAX_NUMBER = 50
 
 def is_prime(number: int):
     k = 0
-    for i in range(2, number ** 0.5 + 1):
+    for i in range(2, number // 2 + 1):
         if (number % i == 0):
             k = k + 1
     if (k <= 0):
